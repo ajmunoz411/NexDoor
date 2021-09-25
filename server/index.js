e54e969626e0e7f414d65b70020811bb01451496
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-24 23:49:20 -0700

    Router refactor to match modules

--- server/index.js
+++ server/index.js
@@ -2,28 +2,28 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const io = require('socket.io')(3000, {
+const io = require('socket.io')(3200, {
   cors: {
     origin: '*',
   },
 });
-// creates session object
+
 const session = require('express-session');
-const router = require('./router');
 require('dotenv').config();
 const redis = require('redis');
 const connectRedis = require('connect-redis');
+const router = require('./router');
 
 const app = express();
 const port = 3500;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(morgan('dev'));
 app.use(cors({ origin: `http://localhost:${process.env.CLIENT_PORT}`, credentials: true }));
 
 app.set('trust proxy', 1); // enable if using proxy/load balancer
+
 const RedisStore = connectRedis(session);
 
 const redisClient = redis.createClient({
@@ -31,11 +31,10 @@ const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
 });
 
-// generates new session every time endpoint is hit (used as middleware)
 app.use(session({
   store: new RedisStore({ client: redisClient }),
   secret: process.env.SECRET,
-  resave: true, // DO NOT DISABLE OR ELSE SESSIONS WONT BE SAVED WITH USER ID
+  resave: true, // DO NOT DISABLE OR SESSIONS WONT BE SAVED WITH USER ID
   saveUninitialized: true,
   cookie: {
     secure: false, // HTTPS only (disable in production)
@@ -53,13 +52,8 @@ app.listen(port, () => {
 app.use(express.static(path.join(__dirname, '..', 'client/index')));
 
 io.on('connection', (socket) => {
-  console.log('user connected');
-  // socket.on('join', (room) => {
-  //   socket.join(room);
-  // });
   socket.on('send-message', ({ task, message }) => {
     console.log(task);
     socket.broadcast.emit(task, message);
-    // socket.to('room1').emit(message);
   });
 });
--- server/router.js
+++ server/router.js
@@ -1,64 +1,19 @@
 /* eslint-disable indent */
 const router = require('express').Router();
-const announceCtrl = require('./controllers/announceCtrl');
-const messageCtrl = require('./controllers/messageCtrl');
-const taskCtrl = require('./controllers/taskCtrl');
-const userCtrl = require('./controllers/userCtrl');
+
+const announcements = require('./announcements/routes');
+const messages = require('./messages/routes');
+const tasks = require('./tasks/routes');
+const users = require('./users/routes');
+const login = require('./login/login');
+const newuser = require('./login/newuser');
 
 router
-  // ANNOUNCEMENTS---------------------------------------------------
-    // GET
-    .get('/announce/:quantity', announceCtrl.getAnnouncements)
-    // ADD / UPDATE
-    .post('/announce/:userId', announceCtrl.addAnnouncement)
-  // MESSAGES--------------------------------------------------------
-    // GET
-    .get('/messages/:taskId', messageCtrl.getMessagesByTask)
-    // ADD / UPDATE
-    .post('/messages/:taskId/:userId', messageCtrl.addMessage)
-  // TASKS-----------------------------------------------------------
-    // GET
-    .get('/tasks/master/:userId/:range/:quantity/:offset', taskCtrl.getTasksMasterDefaultAddress)
-    .get('/tasks/altmaster/:userId/:range/:quantity/:offset', taskCtrl.getTasksMasterAltAddress)
-    .get('/tasks/all/:userId/:quantity/:offset', taskCtrl.getTasks)
-    .get('/tasks/req/:userId', taskCtrl.getReqTasksByUser)
-    .get('/tasks/help/:userId', taskCtrl.getHelpTasksByUser)
-    .get('/tasks/range/:userId/:range', taskCtrl.getTasksInRange)
-    .get('/tasks/alt/:range', taskCtrl.getTasksInRangeAltAddress)
-    // UPDATE
-    .put('/task/help/:taskId/:userId', taskCtrl.updateHelper)
-    .put('/task/rmhelp/:taskId', taskCtrl.removeHelper)
-    .put('/task/change/:status/:taskId', taskCtrl.changeTaskStatus)
-    .put('/task/close/:taskId/:rating', taskCtrl.closeTask)
-    .put('/task/edit', taskCtrl.editTask)
-    // ADD
-    .post('/task/check/:userId', taskCtrl.addTaskCheckAddress)
-    .post('/task/new/:userId', taskCtrl.addTaskNewAddress)
-    .post('/task/home/:userId', taskCtrl.addTaskHomeAddress)
-    // DELETE
-    .delete('/task/:taskId', taskCtrl.deleteTask)
-  // USERS----------------------------------------------------------
-    // GET
-    .get('/user/info/:userId', userCtrl.getUser)
-    .get('/users/rating/:quantity', userCtrl.getUsersByRating)
-    .get('/users/rangerating/:quantity/:userId/:range', userCtrl.getUsersInRangeByRating)
-    // ADD
-    .post('/user', userCtrl.addUser)
-    // UPDATE
-    // .put('/user/:userId', userCtrl.updateUser)
-    // DELETE
-    // .delete('/user/:userId', userCtrl.deleteUser)
-  // LOGIN ----------------------------------------------------------
-    .get('/credentials/:userId', userCtrl.getUserCredentials)
-    .get('/email', userCtrl.checkForEmail)
-    .get('/session', userCtrl.authenticateSession);
-
-// import individual routes
-const login = require('./routes/login');
-const newuser = require('./routes/newuser');
-
-// route each endpoint
-router.use('/login', login);
-router.use('/newuser', newuser);
+  .use('/announce', announcements)
+  .use('/messages', messages)
+  .use('/tasks', tasks)
+  .use('/users', users)
+  .use('/login', login)
+  .use('/newuser', newuser);
 
 module.exports = router;
