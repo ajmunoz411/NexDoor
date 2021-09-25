aab972e2168b9a040f4a1f9c029abd9f5a9bc3e2
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-24 20:14:55 -0700

    Tasks refactor to module, addTaskNewAddress, addTaskHomeAddress, getTasks complete

---
+++ server/tasks/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const taskCtrl = require('./controllers');
+
+const tasks = express.Router();
+
+tasks
+  .get('/master/:userId/:range/:quantity/:offset', taskCtrl.getTasksMasterDefaultAddress)
+  .get('/altmaster/:userId/:range/:quantity/:offset', taskCtrl.getTasksMasterAltAddress)
+  .get('/all/:userId/:quantity/:offset', taskCtrl.getTasks)
+  .get('/req/:userId', taskCtrl.getReqTasksByUser)
+  .get('/help/:userId', taskCtrl.getHelpTasksByUser)
+  .get('/range/:userId/:range', taskCtrl.getTasksInRange)
+  .get('/alt/:range', taskCtrl.getTasksInRangeAltAddress)
+
+  .put('/help/:taskId/:userId', taskCtrl.updateHelper)
+  .put('/rmhelp/:taskId', taskCtrl.removeHelper)
+  .put('/change/:status/:taskId', taskCtrl.changeTaskStatus)
+  .put('/close/:taskId/:rating', taskCtrl.closeTask)
+  .put('/edit', taskCtrl.editTask)
+
+  // .post('/check/:userId', taskCtrl.addTaskCheckAddress)
+  .post('/new/:userId', taskCtrl.addTaskNewAddress)
+  .post('/home/:userId', taskCtrl.addTaskHomeAddress)
+
+  .delete('/:taskId', taskCtrl.deleteTask);
+
+module.exports = tasks;
